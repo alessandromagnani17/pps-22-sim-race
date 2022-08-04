115e4cc76e928bebd155b0d6567250f7e652e56d
dom <davidedomini@gmail.com>
2022-08-04 10:38:53 +0200

    refactor(TrackBuilder): refactored code for creating left turn

--- src/main/scala/it/unibo/pps/model/TrackBuilder.scala
+++ src/main/scala/it/unibo/pps/model/TrackBuilder.scala
@@ -44,11 +44,32 @@ object TrackBuilder:
       val y2I = h2
       DrawingTurnParams((x0E, y0E), (x1E, y1E), (x1I, y1I), (x2E, y2E), (x2I, y2I), direction)
 
+    private def computeTurnLeft(w: Int, h: Int, direction: Int): DrawingTurnParams =
+      val w1 = (0.3 * w).toInt
+      var h1 = (0.3 * h).toInt
+      var h2 = (0.7 * h).toInt
+      val x0E = w1 //Da cambiare metto il riferimento a w1
+      val y0E = (h1 + h2) / 2 //Da cambiare metto il riferimento a w1
+      val x1E = w1
+      val x2E = w1
+      val y1E = h1
+      val y2E = h2
+      h1 = (0.40 * h).toInt
+      h2 = (0.60 * h).toInt
+      val x0I = w1 //Da cambiare metto il riferimento a w1
+      val y0I = (h1 + h2) / 2 //Da cambiare metto il riferimento a w1
+      val x1I = w1
+      val x2I = w1
+      val y1I = h1
+      val y2I = h2
+      DrawingTurnParams((x0E, y0E), (x1E, y1E), (x1I, y1I), (x2E, y2E), (x2I, y2I), direction)
+
     override def createBaseTrack(w: Int, h: Int): Track =
       val track = Track()
       track.addSector(Sector.Straight(1, computeStraightUp(w, h)))
       track.addSector(Sector.Turn(2, computeTurnRight(w, h, 1)))
       track.addSector(Sector.Straight(3, computeStraightDown(w, h)))
+      track.addSector(Sector.Turn(4, computeTurnLeft(w, h, -1)))
       track
 /*
       var w1 = (0.3 * w).toInt
