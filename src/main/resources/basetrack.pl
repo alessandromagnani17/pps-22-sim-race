f6b17c938f51d0a97957fc5e0553553e1b06fed9
dom <davidedomini@gmail.com>
2022-08-05 15:28:00 +0200

    feat(BaseTrack.pl): added functors to identify different fields of sector record

--- src/main/resources/basetrack.pl
+++ src/main/resources/basetrack.pl
@@ -4,15 +4,16 @@
 % I ---> Internal
 
 
-% Straight(id, x0_E, y0_E, x1_E, y1_E, x0_I, y0_I, x1_I, y1_I)
-straight(1, 272, 170, 634, 170, 272, 226, 634, 226).
-straight(3, 272, 396, 634, 396, 272, 340, 634, 340).
-
+%straight(id, x0_E, y0_E, x1_E, y1_E, x0_I, y0_I, x1_I, y1_I)
+%straight(id(ID), startPointE(X0_E, Y0_E), endPointE(X1_E, Y1_E), startPointI(X0_I, Y0_I), endPointI(X1_I, Y1_I))
+straight(id(1), startPointE(272, 170), endPointE(634, 170), startPointI(272, 226), endPointI(634, 226)).
+straight(id(3), startPointE(272, 396), endPointE(634, 396), startPointI(272, 340), endPointI(634, 340)).
 
 %Direction ---> -1 or 1
 %SP ---> Start Point
 %EP ---> End Point
 
-% Turn(id, x_SP_E, y_SP_E, x_SP_I, y_SP_I, x_EP_E, y_EP_E, x_EP_I, y_EP_I,direction)
-turn(2, 634, 283, 634, 170, 634, 226, 634, 396, 634, 340, 1).
-turn(4, 272, 283, 272, 170, 272, 226, 272, 396, 272, 340, -1).
+%turn(id, x_center, y_center, x_SP_E, y_SP_E, x_SP_I, y_SP_I, x_EP_E, y_EP_E, x_EP_I, y_EP_I,direction)
+%Query: turn(id(ID), center(X, Y), startPointE(X0_E, Y0_E), startPointI(X0_I, YO_I), endPointE(X1_E, Y1_E), endPointI(X1_I, Y1_I), direction(D))
+turn(id(2), center(634, 283), startPointE(634, 170), startPointI(634, 226), endPointE(634, 396), endPointI(634, 340), direction(1)).
+turn(id(4), center(272, 283), startPointE(272, 170), startPointI(272, 226), endPointE(272, 396), endPointI(272, 340), direction(-1)).
