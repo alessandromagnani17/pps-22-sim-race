7bd9ed92036d02082e555cf185fa7535f4c8fdf5
Simone Montanari <simone.montanari14@studio.unibo.it>
2022-08-06 10:27:12 +0200

    refactor(initial panel): refactored methods of arrow buttons

--- src/main/scala/it/unibo/pps/view/InitialLeftPanel.scala
+++ src/main/scala/it/unibo/pps/view/InitialLeftPanel.scala
@@ -24,34 +24,26 @@ object InitialLeftPanel:
     self =>
 
     private var currentCarIndex = 0
+    private val colorNotSelected = Color(238, 238, 238)
+    private val colorSelected = Color(79, 195, 247)
+    private val numCars = 4
+
     private val mapCarNames: Map[Int, String] = Map(0 -> "Ferrari", 1 -> "Mercedes", 2 -> "Red Bull", 3 -> "McLaren")
 
     private val initialLeftPanel = createPanel()
 
     private val carSelectedLabel = createJLabel("Car selected: " + mapCarNames(currentCarIndex))
 
-    private val topArrowButton = createTopArrowButton("src/main/resources/arrows/arrow-up.png")
-    private val bottomArrowButton = createBottomArrowButton("src/main/resources/arrows/arrow-bottom.png")
+    private val topArrowButton = createArrowButton("src/main/resources/arrows/arrow-up.png", e => if (e + 1) == numCars then 0.toString else (e + 1).toString)
+    private val bottomArrowButton = createArrowButton("src/main/resources/arrows/arrow-bottom.png", e => if (e - 1) < 0 then (numCars - 1).toString else (e - 1).toString)
 
     private val labelImage = createLabelImage("src/main/resources/cars/0-hard.png", "0")
-    /*private val labelImage2 = createLabelImage("src/main/resources/cars/1.png", "1")
-    private val labelImage3 = createLabelImage("src/main/resources/cars/2.png", "2")
-    private val labelImage4 = createLabelImage("src/main/resources/cars/3.png", "3")
-
-    private val labelImages = List(labelImage1, labelImage2, labelImage3, labelImage4)*/
-
-    private val colorNotSelected = Color(238, 238, 238)
-    private val colorSelected = Color(79, 195, 247)
-    private val numCars = 4
 
     controller.setCurrentCarIndex(currentCarIndex)
 
     initialLeftPanel foreach(e => self.add(e))
 
     def changeCar(carIndex: Int, tyresType: String): Unit = labelImage.foreach(e => e.setIcon(ImageIcon("src/main/resources/cars/" + carIndex + "-" + tyresType + ".png")))
-
-    // Fai vedere la macchina con indice carIndex e le gomme tyresType
-
 
     private def createLabelImage(filename: String, name: String): Task[JLabel] =
       for
@@ -69,40 +61,22 @@ object InitialLeftPanel:
         _ <- label.setHorizontalAlignment(SwingConstants.CENTER)
       yield label
 
-    private def createTopArrowButton(filename: String): Task[JButton] =
+    private def createArrowButton(path: String, calcIndex: Int => String): Task[JButton] =
       for
-        button <- JButton(ImageIcon(filename))
+        button <- JButton(ImageIcon(path))
         _ <- button.setBorder(BorderFactory.createEmptyBorder())
         _ <- button.setBackground(colorNotSelected)
         _ <- button.setVerticalAlignment(SwingConstants.BOTTOM)
         _ <- button.addActionListener(new ActionListener {
           override def actionPerformed(e: ActionEvent): Unit =
-            val nextIndex = if (currentCarIndex + 1) == numCars then 0.toString else (currentCarIndex + 1).toString
-            
+            val nextIndex = calcIndex(currentCarIndex)
+
             controller.setCurrentCarIndex(nextIndex.toInt)
             changeCar(nextIndex.toInt, "hard")
             currentCarIndex = nextIndex.toInt
-            carSelectedLabel.foreach(e => e.setText("Car selected: " + mapCarNames(currentCarIndex)))
+            carSelectedLabel.foreach(e => e.setText(s"Car selected: ${mapCarNames(currentCarIndex)}"))
         })
       yield button
-
-    private def createBottomArrowButton(filename: String): Task[JButton] =
-      for
-        button <- JButton(ImageIcon(filename))
-        _ <- button.setBorder(BorderFactory.createEmptyBorder())
-        _ <- button.setBackground(colorNotSelected)
-        _ <- button.setVerticalAlignment(SwingConstants.TOP)
-        _ <- button.addActionListener(new ActionListener {
-          override def actionPerformed(e: ActionEvent): Unit =
-            val prevIndex = if (currentCarIndex - 1) < 0 then (numCars - 1).toString else (currentCarIndex - 1).toString
-            
-            controller.setCurrentCarIndex(prevIndex.toInt)
-            changeCar(prevIndex.toInt, "hard")
-            currentCarIndex = prevIndex.toInt
-            carSelectedLabel.foreach(e => e.setText("Car selected: " + mapCarNames(currentCarIndex)))
-        })
-      yield button
-
 
     private def createPanel(): Task[JPanel] =
       for
