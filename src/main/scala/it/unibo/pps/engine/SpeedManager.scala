2ec0611ce58cc9f3c54aa6f03cf309eaf880a4df
dom <davidedomini@gmail.com>
2022-08-09 10:43:16 +0200

    doc: added scaladoc in speed manager

--- src/main/scala/it/unibo/pps/engine/SpeedManager.scala
+++ src/main/scala/it/unibo/pps/engine/SpeedManager.scala
@@ -3,8 +3,20 @@ package it.unibo.pps.engine
 import it.unibo.pps.engine.SimulationConstants.*
 
 trait SpeedManager:
+
+  /** @return the actual simulation speed */
   def _simulationSpeed: Double
+
+  /** Method that decreases the actual simulation speed
+    *
+    * If the speed is already at the minimum value it does nothing
+    */
   def decreaseSpeed(): Unit
+
+  /** Method that increases the actual simulation speed
+    *
+    * If the speed is already at the maximum value it does nothing
+    */
   def increaseSpeed(): Unit
 
 object SpeedManager:
