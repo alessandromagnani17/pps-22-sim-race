529dcc133e71721c9f2cefa9fd742aefce3d7254
alessandromagnani17 <86307000+alessandromagnani17@users.noreply.github.com>
2022-08-12 16:25:51 +0200

    feat(car model): added labels to starting positions frame

--- src/main/scala/it/unibo/pps/view/StartingPositionsPanel.scala
+++ src/main/scala/it/unibo/pps/view/StartingPositionsPanel.scala
@@ -3,8 +3,8 @@ package it.unibo.pps.view
 import it.unibo.pps.controller.ControllerModule
 import monix.eval.Task
 
-import java.awt.{Dimension, FlowLayout}
-import javax.swing.{JLabel, JPanel}
+import java.awt.{Color, Dimension, FlowLayout, GridBagConstraints, GridBagLayout}
+import javax.swing.{BorderFactory, JButton, JLabel, JPanel, SwingConstants}
 import it.unibo.pps.utility.GivenConversion.GuiConversion.given
 import monix.execution.Scheduler.Implicits.global
 
@@ -17,14 +17,47 @@ object StartingPositionsPanel:
   private class StartingPositionsPanelImpl(width: Int, height: Int, controller: ControllerModule.Controller)
     extends StartingPositionsPanel:
     self =>
+
+    // mettere costante
+    private val numCars = 4
+    private val carNames: Map[Int, String] = Map(0 -> "Ferrari", 1 -> "Mercedes", 2 -> "Red Bull", 3 -> "McLaren")
+    private val labelList = createLabels()
+    private val labelHeight = (height - 60) / numCars
     private val startingPositionsPanel = createPanelAndAddAllComponents()
     startingPositionsPanel foreach (e => self.add(e))
+
+    private def createLabels(): List[Task[JLabel]] =
+      val labels = for
+        index <- 0 until numCars
+        label = createLabel(index)
+      yield label
+      labels.toList
+
+    private def createLabel(index: Int): Task[JLabel] =
+      for
+        label <- JLabel(carNames(index))
+        _ <- label.setName(index.toString)
+        _ <- label.setPreferredSize(Dimension(width, labelHeight))
+        _ <- label.setHorizontalAlignment(SwingConstants.CENTER)
+        _ <- label.setVerticalAlignment(SwingConstants.CENTER)
+        _ <- label.setBorder(BorderFactory.createMatteBorder(0,0,1,0, Color.BLACK))
+      yield label
 
     private def createPanelAndAddAllComponents(): Task[JPanel] =
       for
         panel <- JPanel()
-        _ <- println("creo")
         _ <- panel.setPreferredSize(Dimension(width, height))
         _ <- panel.setLayout(FlowLayout())
+        _ <- addLabelsToPanel(labelList, panel)
         _ <- panel.setVisible(true)
       yield panel
+
+    private def addLabelsToPanel(labels: List[Task[JLabel]], panel: JPanel): Task[Unit] =
+      for task <- labels do addLabel(task, panel)
+
+    private def addLabel(task: Task[JLabel], panel: JPanel): Task[Unit] =
+      val p = for
+        label <- task
+        _ <- panel.add(label)
+      yield ()
+      p.runSyncUnsafe()
