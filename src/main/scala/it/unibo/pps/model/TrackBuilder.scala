daa9383be02c5e4c0d3d8b7b454dd717e5990869
dom <davidedomini@gmail.com>
2022-08-14 10:58:16 +0200

    refactor: refactored TrackBuilder class

--- src/main/scala/it/unibo/pps/model/TrackBuilder.scala
+++ src/main/scala/it/unibo/pps/model/TrackBuilder.scala
@@ -30,16 +30,14 @@ object TrackBuilder:
 
     private val engine = Scala2P.createEngine("/prolog/basetrack.pl")
 
-    private def mkStraight(l: List[String]): Sector.Straight = l match {
-      case List(id, xP0Ex, yP0Ex, xP1Ex, yP1Ex, xP0In, yP0In, xP1In, yP1In) =>
-        val d = DrawingStraightParams(
-          (xP0Ex, yP0Ex),
-          (xP1Ex, yP1Ex),
-          (xP0In, yP0In),
-          (xP1In, yP1In)
-        )
-        Sector.Straight(id, d)
-    }
+    override def createBaseTrack(): Track =
+      val track = Track()
+      loadStraights().foreach(track.addSector(_))
+      loadTurns().foreach(track.addSector(_))
+      loadStartingGrid().foreach(
+        track.addStartingPoint(_)
+      )
+      track
 
     private def loadStraights(): List[Sector.Straight] =
       val l = List("ID", "X0_E", "Y0_E", "X1_E", "Y1_E", "X0_I", "Y0_I", "X1_I", "Y1_I")
@@ -50,6 +48,37 @@ object TrackBuilder:
         x = Scala2P.extractTermsToListOfStrings(s, l)
         straight = mkStraight(x)
       yield straight
+
+    private def loadTurns(): List[Sector.Turn] =
+      val l = List("ID", "X", "Y", "X0_E", "Y0_E", "X0_I", "Y0_I", "X1_E", "Y1_E", "X1_I", "Y1_I", "D")
+      for
+        s <- engine(
+          "turn(id(ID), center(X, Y), startPointE(X0_E, Y0_E), startPointI(X0_I, Y0_I), endPointE(X1_E, Y1_E), endPointI(X1_I, Y1_I), direction(D))"
+        )
+        x = Scala2P.extractTermsToListOfStrings(s, l)
+        turn = mkTurn(x)
+      yield turn
+
+    private def loadStartingGrid(): List[StartingPoint] =
+      val l = List("ID", "X_POSITION", "Y_POSITION")
+      for
+        s <- engine(
+          "startingPoint(id(ID), position(X_POSITION, Y_POSITION))"
+        )
+        x = Scala2P.extractTermsToListOfStrings(s, l)
+        startingPoint = mkStartingPoint(x)
+      yield startingPoint
+
+    private def mkStraight(l: List[String]): Sector.Straight = l match {
+      case List(id, xP0Ex, yP0Ex, xP1Ex, yP1Ex, xP0In, yP0In, xP1In, yP1In) =>
+        val d = DrawingStraightParams(
+          (xP0Ex, yP0Ex),
+          (xP1Ex, yP1Ex),
+          (xP0In, yP0In),
+          (xP1In, yP1In)
+        )
+        Sector.Straight(id, d)
+    }
 
     private def mkTurn(l: List[String]): Sector.Turn = l match {
       case List(id, x_center, y_center, x_SP_E, y_SP_E, x_SP_I, y_SP_I, x_EP_E, y_EP_E, x_EP_I, y_EP_I, direction) =>
@@ -64,35 +93,6 @@ object TrackBuilder:
         Sector.Turn(id, d)
     }
 
-    private def loadTurns(): List[Sector.Turn] =
-      val l = List("ID", "X", "Y", "X0_E", "Y0_E", "X0_I", "Y0_I", "X1_E", "Y1_E", "X1_I", "Y1_I", "D")
-      for
-        s <- engine(
-          "turn(id(ID), center(X, Y), startPointE(X0_E, Y0_E), startPointI(X0_I, Y0_I), endPointE(X1_E, Y1_E), endPointI(X1_I, Y1_I), direction(D))"
-        )
-        x = Scala2P.extractTermsToListOfStrings(s, l)
-        turn = mkTurn(x)
-      yield turn
-
     private def mkStartingPoint(l: List[String]): StartingPoint = l match {
       case List(id, x, y) => StartingPoint(id, DrawingStartingPointParams((x, y)))
     }
-
-    private def loadStartingGrid(): List[StartingPoint] =
-      val l = List("ID", "X_POSITION", "Y_POSITION")
-      for
-        s <- engine(
-          "startingPoint(id(ID), position(X_POSITION, Y_POSITION))"
-        )
-        x = Scala2P.extractTermsToListOfStrings(s, l)
-        startingPoint = mkStartingPoint(x)
-      yield startingPoint
-
-    override def createBaseTrack(): Track =
-      val track = Track()
-      loadStraights().foreach(track.addSector(_))
-      loadTurns().foreach(track.addSector(_))
-      loadStartingGrid().foreach(
-        track.addStartingPoint(_)
-      )
-      track
