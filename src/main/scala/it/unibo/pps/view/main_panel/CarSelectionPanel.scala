b8d88c1c6c0bda429a0f8c7885ccb2d0ecdd70bd
Simone Montanari <simone.montanari14@studio.unibo.it>
2022-08-14 15:47:34 +0200

    feat(initial panel): refactored code

--- src/main/scala/it/unibo/pps/view/main_panel/CarSelectionPanel.scala
+++ src/main/scala/it/unibo/pps/view/main_panel/CarSelectionPanel.scala
@@ -27,7 +27,7 @@ object CarSelectionPanel:
     private val colorSelected = Color(79, 195, 247)
     private val numCars = 4
     private val carNames: Map[Int, String] = Map(0 -> "Ferrari", 1 -> "Mercedes", 2 -> "Red Bull", 3 -> "McLaren")
-    private val carSelectedLabel = createLabel(s"Car selected: ${carNames(0)}")
+    private val carSelectedLabel = createLabel(s"Car selected: ${carNames(0)}", Dimension(width, (height * 0.2).toInt), SwingConstants.CENTER, SwingConstants.CENTER, false)
     private val topArrowButton = createArrowButton(
       "/arrows/arrow-up.png",
       e => if (e + 1) == numCars then 0.toString else (e + 1).toString
@@ -36,7 +36,7 @@ object CarSelectionPanel:
       "/arrows/arrow-bottom.png",
       e => if (e - 1) < 0 then (numCars - 1).toString else (e - 1).toString
     )
-    private val labelImage = createLabelImage("/cars/0-hard.png")
+    private val labelImage = createLabel("/cars/0-hard.png", Dimension(width, (height * 0.35).toInt), SwingConstants.CENTER, 9, true)
     private val carSelectionPanel = createPanelAndAddAllComponents()
 
     carSelectionPanel foreach (e => self.add(e))
@@ -44,19 +44,12 @@ object CarSelectionPanel:
     def updateDisplayedCar(): Unit =
       labelImage.foreach(e => e.setIcon(imageLoader.load(controller.getCurrentCar().path)))
 
-    private def createLabelImage(filename: String): Task[JLabel] =
+    private def createLabel(text: String, dimension: Dimension, vertical: Int, horizontal: Int, isImage: Boolean): Task[JLabel] =
       for
-        label <- JLabel(imageLoader.load(filename))
-        _ <- label.setPreferredSize(Dimension(width, (height * 0.35).toInt))
-        _ <- label.setVerticalAlignment(SwingConstants.CENTER)
-      yield label
-
-    private def createLabel(text: String): Task[JLabel] =
-      for
-        label <- JLabel(text)
-        _ <- label.setPreferredSize(Dimension(width, (height * 0.2).toInt))
-        _ <- label.setVerticalAlignment(SwingConstants.CENTER)
-        _ <- label.setHorizontalAlignment(SwingConstants.CENTER)
+        label <- if isImage then JLabel(imageLoader.load(text)) else JLabel(text)
+        _ <- label.setPreferredSize(dimension)
+        _ <- label.setVerticalAlignment(vertical)
+        _ <- if !isImage then label.setHorizontalAlignment(horizontal)
       yield label
 
     private def createArrowButton(path: String, calcIndex: Int => String): Task[JButton] =
