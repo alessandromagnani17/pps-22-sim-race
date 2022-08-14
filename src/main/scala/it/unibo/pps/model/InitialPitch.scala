3187dd7e0b9c3ae7d7e152c76ea27f4f8cdc2b96
dom <davidedomini@gmail.com>
2022-08-14 10:03:45 +0200

    refactor: renamed InitialPitch in StartingPoint and changed its type from enum to case class

--- src/main/scala/it/unibo/pps/model/InitialPitch.scala
+++ src/main/scala/it/unibo/pps/model/InitialPitch.scala
@@ -2,6 +2,9 @@ package it.unibo.pps.model
 
 import it.unibo.pps.view.DrawingCarParams
 
-enum InitialPitch:
+/*enum InitialPitch:
   case listOfPitches(id: Int, drawingParams: DrawingCarParams)
-  
+
+ */
+
+case class StartingPoint(id: Int)
