897f126023483d6e538ec8e2600337d6f05ba669
Simone Montanari <simone.montanari14@studio.unibo.it>
2022-08-17 14:50:54 +0200

    feat(car model): refactored code

--- src/main/scala/it/unibo/pps/model/Car.scala
+++ src/main/scala/it/unibo/pps/model/Car.scala
@@ -4,20 +4,3 @@ import javax.swing.Icon
 import it.unibo.pps.view.simulation_panel.DrawingCarParams
 
 case class Car(var path: String, name: String, var tyre: Tyre, driver: Driver, var maxSpeed: Int, drawingCarParams: DrawingCarParams)
-
-/*
-trait Car:
-  def getName(): String
-  def getTyreType(): Tyre
-  def setTyreType(tyreType: Tyre): Unit
-
-object Car:
-  def apply(name: String, tyreType: Tyre, driver: Driver, maxSpeed: Int): Car = new CarImpl(name, tyreType, driver, maxSpeed)
-
-  private class CarImpl(name: String, tyreType: Tyre, driver: Driver, maxSpeed: Int) extends Car:
-
-    override def getName(): String = name
-    override def getTyreType(): Tyre = tyreType
-
-    override def setTyreType(tyre: Tyre): Unit = tyreType = tyre
-*/
