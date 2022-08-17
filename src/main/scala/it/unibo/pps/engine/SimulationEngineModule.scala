90d4a12f2fa53d1e1cac5835c6bc242eae9c8904
dom <davidedomini@gmail.com>
2022-08-17 18:09:43 +0200

    feat(simulation engine): implemented a first version of calculation of new position in the straight with prolog

--- src/main/scala/it/unibo/pps/engine/SimulationEngineModule.scala
+++ src/main/scala/it/unibo/pps/engine/SimulationEngineModule.scala
@@ -1,10 +1,10 @@
 package it.unibo.pps.engine
 
+import alice.tuprolog.{Term, Theory}
 import it.unibo.pps.model.{Car, ModelModule, Snapshot}
 import it.unibo.pps.view.ViewModule
 import monix.eval.Task
 import monix.execution.Scheduler
-
 import concurrent.duration.{Duration, DurationDouble, DurationInt, FiniteDuration}
 import scala.language.postfixOps
 import it.unibo.pps.engine.SimulationConstants.*
@@ -12,6 +12,10 @@ import it.unibo.pps.prolog.Scala2P
 import it.unibo.pps.utility.monadic.*
 import it.unibo.pps.utility.GivenConversion.ModelConversion
 import it.unibo.pps.view.simulation_panel.DrawingCarParams
+
+given Conversion[String, Term] = Term.createTerm(_)
+given Conversion[Seq[_], Term] = _.mkString("[", ",", "]")
+given Conversion[String, Theory] = Theory.parseLazilyWithStandardOperators(_)
 
 object SimulationEngineModule:
   trait SimulationEngine:
@@ -66,11 +70,24 @@ object SimulationEngineModule:
           newCars = for
             car <- cars
             oldPosition = car.drawingCarParams.position
-            p = (oldPosition._1 + 2, oldPosition._2)
+            oldVel = car.velocity
+            oltTime = snapshot.time
+            newX = calcWithProlog(oldPosition._1, oltTime + 1, oldVel)
+            p = (newX, oldPosition._2)
             d = DrawingCarParams(p, car.drawingCarParams.color)
           yield Car(car.path, car.name, car.tyre, car.driver, car.maxSpeed, car.velocity, d)
           newSnap <- io(Snapshot(newCars.toList, time + 1))
         yield newSnap
+
+      private def calcWithProlog(x: Int, time: Int, velocity: Double): Int =
+        val acceleration =
+          2 //TODO - ora è in pixel/s^2 --> bisogna mettere a posto le unità di misura (la vel è in km/h)
+        engine(s"computeNewPosition($x, $velocity, $time, $acceleration, Np)")
+          .map(Scala2P.extractTermToString(_, "Np"))
+          .toSeq
+          .head
+          .toDouble
+          .toInt
 
       private def getLastSnapshot(): Task[Snapshot] =
         io(context.model.getLastSnapshot())
