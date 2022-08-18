787161a5dc4846fc262d8cf07f83d593ce493db2
Simone Montanari <simone.montanari14@studio.unibo.it>
2022-08-18 15:19:34 +0200

    feat(car model): added snapshot to model module

--- src/main/scala/it/unibo/pps/model/Car.scala
+++ src/main/scala/it/unibo/pps/model/Car.scala
@@ -3,4 +3,22 @@ package it.unibo.pps.model
 import javax.swing.Icon
 import it.unibo.pps.view.simulation_panel.DrawingCarParams
 
-case class Car(var path: String, name: String, var tyre: Tyre, driver: Driver, var maxSpeed: Int, drawingCarParams: DrawingCarParams)
+case class Car(
+                var path: String,
+                name: String,
+                var tyre: Tyre,
+                driver: Driver,
+                var maxSpeed: Int,
+                var velocity: Double,
+                drawingCarParams: DrawingCarParams
+              )
+
+/*
+object Car:
+  def computeNewVelocity(acceleration: Double , body: Car, t: Double): Double = body.velocity + acceleration * t
+
+  def computeNewPosition(acceleration: Double , body: Car, t: Double): Tuple2[Double, Double] =
+    (body.drawingCarParams.position._1 + body.velocity * t + 0.5 * acceleration * (t*t), body.drawingCarParams.position._2)
+
+  def dummyNewPosition(oldPosition: Tuple2[Double, Double]) = (oldPosition._1 + 2, oldPosition._2)
+ */
