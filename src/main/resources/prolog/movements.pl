447ce258005ef7d15871009def700f8400cd1b04
dom <davidedomini@gmail.com>
2022-08-29 15:37:43 +0200

    refactor: refactored movements.pl

--- src/main/resources/prolog/movements.pl
+++ src/main/resources/prolog/movements.pl
@@ -6,7 +6,7 @@ computeNewVelocity(Speed, Acc, Time, Deg, Fuel, Ns) :- computeZ(Deg, Fuel, Z), T
 
 computeZ(Deg, Fuel, Z) :- Z is (Deg + Fuel) / 180.
 
-computeNewVelocityDeceleration(Speed, Acc, Time, Ns) :- Ns is (Speed + (Acc * Time))*0.3.
+computeNewVelocityDeceleration(Speed, Acc, Time, Deg, Fuel, Ns) :- computeZ(Deg, Fuel, Z), Temp is (Speed + (Acc * Time)), Ns is (Temp - (Temp * Z))*0.9.
 
 pow(X, Esp, Y):-  pow(X, X, Esp, Y).
 pow(X, Temp, Esp, Y):- Esp=:=0, !, Y=1.
--- src/main/scala/it/unibo/pps/engine/PrologMovements.scala
+++ src/main/scala/it/unibo/pps/engine/PrologMovements.scala
@@ -39,7 +39,8 @@ object PrologMovements:
         .toInt
 
     override def newVelocityStraightDec(car: Car, time: Int): Int =
-      engine(s"computeNewVelocityDeceleration(${car.actualSpeed}, 1, $time,  Ns)")
+      //println(s"${car.name} ----- maxspeed: ${car.maxSpeed} ----- speed: ${car.actualSpeed} ---- $car.")
+      engine(s"computeNewVelocityDeceleration(${car.actualSpeed}, 1, $time, ${car.degradation}, ${car.fuel}, Ns)")
         .map(Scala2P.extractTermToString(_, "Ns"))
         .toSeq
         .head
