047c56b6c982b18cd282b2696f53eeae536bc055
dom <davidedomini@gmail.com>
2022-09-06 10:16:16 +0200

    fix: fixed tyres degradation

--- src/main/scala/it/unibo/pps/model/Tyre.scala
+++ src/main/scala/it/unibo/pps/model/Tyre.scala
@@ -3,9 +3,9 @@ package it.unibo.pps.model
 import it.unibo.pps.model.Tyre.{HARD, MEDIUM, SOFT}
 
 given Conversion[Tyre, Double] = _ match
-  case HARD => 2.0
+  case HARD => 4.0
   case MEDIUM => 3.0
-  case SOFT => 4.0
+  case SOFT => 2.0
 
 enum Tyre:
   case HARD
