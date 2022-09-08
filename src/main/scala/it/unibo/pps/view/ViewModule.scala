0f9296a71005b66a0830d0b87d19288743771f41
alessandromagnani17 <86307000+alessandromagnani17@users.noreply.github.com>
2022-09-08 12:09:53 +0200

    doc: added scala doc in viewmodule

--- src/main/scala/it/unibo/pps/view/ViewModule.scala
+++ src/main/scala/it/unibo/pps/view/ViewModule.scala
@@ -6,15 +6,38 @@ import it.unibo.pps.view.ViewConstants.*
 
 object ViewModule:
   trait View:
+
+    /**  Method that updates the car displayed */
     def updateDisplayedCar(): Unit
+
+    /**  Method that updates the displayed standings */
     def updateDisplayedStandings(): Unit
+
+    /** Method that displays the starting positions panel */
     def displayStartingPositionsPanel(): Unit
+
+    /** Method that updates the displayed parameters when the car displayed is changed */
     def updateParametersPanel(): Unit
+
+    /** Method that displays the simulation panel
+     *   @param track
+     *   The track to be rendered before the display of the simulation panel
+     */
     def displaySimulationPanel(track: Track): Unit
+
+    /** Method that displays the end race panel */
     def displayEndRacePanel(): Unit
+
     def updateCars(cars: List[Car], actualLap: Int, totalLaps: Int): Unit
     def updateCharts(l: List[Snapshot]): Unit
+
+    /** Method that sets enabled the final report button that if pressed, display the end race panel */
     def setFinalReportEnabled(): Unit
+
+    /**  Method that updates the fastest lap icon
+     * @param carName
+     *   The name of the car that has made the fastest lap
+     */
     def updateFastestLapIcon(carName: String): Unit
 
   trait Provider:
