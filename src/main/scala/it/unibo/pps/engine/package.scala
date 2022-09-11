8300916390695d2eedbacb8569bd45379ec6780c
dom <davidedomini@gmail.com>
2022-09-11 09:01:57 +0200

    doc: added scaladoc in engine package object

--- src/main/scala/it/unibo/pps/engine/package.scala
+++ src/main/scala/it/unibo/pps/engine/package.scala
@@ -11,14 +11,29 @@ import scala.{Tuple2 => Point2D}
 
 package object engine:
 
+  /** Computes car radius when it is in a turn
+    * @param d
+    *   Turn render parameters
+    * @param position
+    *   Car position
+    */
   val computeRadius = (d: RenderParams, position: Point2D[Int, Int]) =>
     d match
       case RenderTurnParams(center, _, _, _, _, _, _, _) => center euclideanDistance position
 
+  /** Computes the angle between two points on a circumference using the cosine laws
+    * @param a
+    *   The first point
+    * @param b
+    *   The second point
+    * @param radius
+    *   The circumference radius
+    */
   val angleBetweenPoints = (a: Point2D[Int, Int], b: Point2D[Int, Int], radius: Int) =>
     val distance = a euclideanDistance b
     Math.acos(((2 * radius ** 2) - distance) / (2 * radius ** 2))
 
+  /** Computes the circular arc given radius and angle */
   val circularArc = (teta: Double, radius: Int) => (teta / 360) * 2 * radius * Math.PI
 
   given RunTask[E]: Conversion[Task[E], E] = _.runSyncUnsafe()
